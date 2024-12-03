5e83d5158c27e7568b3bbdbce7203b4159fc99dd
Anthony Cramp <anthony.cramp@gmail.com>
2024-12-03 19:08:03 +1030

    Add Day 03 skeleton

---
+++ day03/src/main.rs
@@ -0,0 +1,55 @@
+const DAY_NUMBER: &str = "03";
+const INPUT: &str = include_str!("../../inputs/day03.txt");
+// const INPUT: &str = "";
+
+fn main() {
+    println!("Day {} Part 1: {:?}", DAY_NUMBER, part1(INPUT));
+    println!("Day {} Part 2: {:?}", DAY_NUMBER, part2(INPUT));
+}
+
+// replace return type as required by the problem
+fn part1(input: &str) -> i32 {
+    0
+}
+
+// replace return type as required by the problem
+fn part2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_INPUT: &str =
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    use super::*;
+    use test_support::test_support::TestCase;
+
+    #[test]
+    fn test_part1() {
+        let test_cases = [TestCase {
+            input: TEST_INPUT,
+            expected: 161,
+        }];
+        for TestCase { input, expected } in test_cases.iter() {
+            assert_eq!(part1(*input), *expected);
+        }
+    }
+
+    #[test]
+    #[ignore = "not yet implemented"]
+    fn test_part2() {
+        let test_cases = [
+            TestCase {
+                input: TEST_INPUT,
+                expected: 123,
+            },
+            TestCase {
+                input: "abc",
+                expected: 345,
+            },
+        ];
+        for TestCase { input, expected } in test_cases.iter() {
+            assert_eq!(part2(*input), *expected);
+        }
+    }
+}
