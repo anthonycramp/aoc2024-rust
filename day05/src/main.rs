88c65f8acfcfd7dcf5b7b5e9e3054cfd09651081
Anthony Cramp <anthony.cramp@gmail.com>
2024-12-05 20:28:48 +1030

    Add solution for Day 5 Part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -24,15 +24,7 @@ fn part1(input: &str) -> i32 {
 
     let mut sum = 0;
     for print_order in &print_orders {
-        let mut valid_print_order = true;
-        for i in 0..(print_order.len() - 1) {
-            for j in i..print_order.len() {
-                let broken_rule = format!("{}|{}", print_order[j], print_order[i]);
-                let rule_broken = rules.contains(&broken_rule.as_str());
-                valid_print_order &= !rule_broken;
-            }
-        }
-        if valid_print_order {
+        if is_valid_print_order(print_order, &rules) {
             let mid_number = print_order[print_order.len() / 2].parse::<i32>().unwrap();
 
             sum += mid_number;
@@ -44,7 +36,61 @@ fn part1(input: &str) -> i32 {
 
 // replace return type as required by the problem
 fn part2(input: &str) -> i32 {
-    0
+    let mut rules_and_print_orders = input.split("\n\n");
+    let rules = rules_and_print_orders
+        .next()
+        .unwrap()
+        .lines()
+        .collect::<Vec<_>>();
+    let print_orders = rules_and_print_orders
+        .next()
+        .unwrap()
+        .lines()
+        .map(|l| l.split(",").collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut sum = 0;
+    for print_order in &print_orders {
+        if !is_valid_print_order(print_order, &rules) {
+            let mut invalid_print_order = print_order.clone();
+
+            'outer: while !is_valid_print_order(&invalid_print_order, &rules) {
+                for i in 0..(invalid_print_order.len() - 1) {
+                    for j in i..invalid_print_order.len() {
+                        let broken_rule =
+                            format!("{}|{}", invalid_print_order[j], invalid_print_order[i]);
+                        let rule_broken = rules.contains(&broken_rule.as_str());
+                        if rule_broken {
+                            let tmp = invalid_print_order[i];
+                            invalid_print_order[i] = invalid_print_order[j];
+                            invalid_print_order[j] = tmp;
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+
+            let mid_number = invalid_print_order[invalid_print_order.len() / 2]
+                .parse::<i32>()
+                .unwrap();
+
+            sum += mid_number;
+        }
+    }
+
+    sum
+}
+
+fn is_valid_print_order(print_order: &Vec<&str>, rules: &Vec<&str>) -> bool {
+    let mut valid_print_order = true;
+    for i in 0..(print_order.len() - 1) {
+        for j in i..print_order.len() {
+            let broken_rule = format!("{}|{}", print_order[j], print_order[i]);
+            let rule_broken = rules.contains(&broken_rule.as_str());
+            valid_print_order &= !rule_broken;
+        }
+    }
+    valid_print_order
 }
 
 #[cfg(test)]
@@ -92,18 +138,11 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_part2() {
-        let test_cases = [
-            TestCase {
-                input: TEST_INPUT,
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
+        let test_cases = [TestCase {
+            input: TEST_INPUT,
+            expected: 123,
+        }];
         for TestCase { input, expected } in test_cases.iter() {
             assert_eq!(part2(*input), *expected);
         }
